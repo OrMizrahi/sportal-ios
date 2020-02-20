644a20ac3e8e15eda4f62f446503394eb55d1def
Lidor Mashiah <46005945+lidorm@users.noreply.github.com>
2020-02-20 12:30:10 +0200

    Update modelSql.swift

--- ScreensProto/Model/modelSql.swift
+++ ScreensProto/Model/modelSql.swift
@@ -24,13 +24,35 @@ class ModelSql {
         User.createUsersTable();
         Team.createTeamsTable();
         CategoriesModel.createCategoriesTable();
+        Post.createPostsTable()
     }
+    
+    func setLastUpdate(name:String, lastUpdated:Int64){
+           var sqlite3_stmt: OpaquePointer? = nil
+           if (sqlite3_prepare_v2(database,"INSERT OR REPLACE INTO LAST_UPADATE_DATE( NAME, DATE) VALUES (?,?);",-1, &sqlite3_stmt,nil) == SQLITE_OK){
 
+               sqlite3_bind_text(sqlite3_stmt, 1, name,-1,nil);
+               sqlite3_bind_int64(sqlite3_stmt, 2, lastUpdated);
 
-    
-   
-    
-    
-   
-    
+               if(sqlite3_step(sqlite3_stmt) == SQLITE_DONE){
+                   print("new row added succefully")
+               }
+           }
+           sqlite3_finalize(sqlite3_stmt)
+       }
+
+       func getLastUpdateDate(name:String)->Int64{
+           var date:Int64 = 0;
+           var sqlite3_stmt: OpaquePointer? = nil
+
+           if (sqlite3_prepare_v2(database,"SELECT * from LAST_UPADATE_DATE where NAME like ?;",-1,&sqlite3_stmt,nil) == SQLITE_OK){
+
+               sqlite3_bind_text(sqlite3_stmt, 1, name,-1,nil);
+               if(sqlite3_step(sqlite3_stmt) == SQLITE_ROW){
+                   date = Int64(sqlite3_column_int64(sqlite3_stmt,1))
+               }
+           }
+           sqlite3_finalize(sqlite3_stmt)
+           return date
+       }
 }
